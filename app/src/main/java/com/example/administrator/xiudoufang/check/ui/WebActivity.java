dd3f26f1244363cee61a4c6030a011913dc50732
wyf <1073040147@qq.com>
2018-09-15 13:15:59 +0800

    [feature]截图功能完成

--- app/src/main/java/com/example/administrator/xiudoufang/check/ui/WebActivity.java
+++ app/src/main/java/com/example/administrator/xiudoufang/check/ui/WebActivity.java
@@ -1,13 +1,18 @@
 package com.example.administrator.xiudoufang.check.ui;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,18 +24,27 @@ import android.webkit.WebViewClient;
 import android.widget.ImageView;
 
 import com.example.administrator.xiudoufang.R;
+import com.example.administrator.xiudoufang.base.GlideApp;
 import com.example.administrator.xiudoufang.base.IActivityBase;
 import com.example.administrator.xiudoufang.base.XiuDouFangApplication;
 import com.example.administrator.xiudoufang.common.utils.LogUtils;
 import com.example.administrator.xiudoufang.common.utils.MediaScanner;
 import com.example.administrator.xiudoufang.common.utils.PreferencesUtils;
 import com.example.administrator.xiudoufang.common.utils.StringUtils;
+import com.example.administrator.xiudoufang.common.utils.ToastUtils;
 import com.example.administrator.xiudoufang.common.widget.ProgressWebView;
+import com.tbruyelle.rxpermissions2.Permission;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by Administrator on 2018/8/31
@@ -47,11 +61,12 @@ public class WebActivity extends AppCompatActivity implements IActivityBase, Vie
         return R.layout.activity_web;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void initView() {
         setTitle("");
         mWebView = findViewById(R.id.web_view);
-
+        mWebView.enableSlowWholeDocumentDraw();
         findViewById(R.id.iv_back_off).setOnClickListener(this);
         findViewById(R.id.iv_forward).setOnClickListener(this);
     }
@@ -61,6 +76,7 @@ public class WebActivity extends AppCompatActivity implements IActivityBase, Vie
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.getSettings().setSupportZoom(true);
         mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setUseWideViewPort(true);
         mWebView.getSettings().setLoadWithOverviewMode(true);
         mWebView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         mWebView.setWebViewClient(new WebViewClient() {
@@ -82,34 +98,53 @@ public class WebActivity extends AppCompatActivity implements IActivityBase, Vie
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.toolbar_screenshot:
-                saveImageToGallery(shotScreen());
+                new RxPermissions(this)
+                        .requestEach(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .subscribe(new Consumer<Permission>() {
+                            @Override
+                            public void accept(Permission permission) throws Exception {
+                                if (permission.granted) {
+                                    saveImageToGallery(shotScreen());
+                                } else {
+                                    ToastUtils.show(WebActivity.this, "请开启权限后重新尝试");
+                                }
+                            }
+                        });
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
 
     private Bitmap shotScreen() {
-        View view = getWindow().getDecorView();
-        view.setDrawingCacheEnabled(true);
-        view.buildDrawingCache();
-        Bitmap bp = android.graphics.Bitmap.createBitmap(view.getDrawingCache(), 0, 0, view.getMeasuredWidth(),
-                view.getMeasuredHeight());
-        view.setDrawingCacheEnabled(false);
-        view.destroyDrawingCache();
-        return bp;
+//        View view = getWindow().getDecorView();
+//        view.setDrawingCacheEnabled(true);
+//        view.buildDrawingCache();
+//        Bitmap bitmap = android.graphics.Bitmap.createBitmap(view.getDrawingCache(), 0, 0, view.getMeasuredWidth(),
+//                view.getMeasuredHeight());
+//        view.setDrawingCacheEnabled(false);
+//        view.destroyDrawingCache();
+
+        float scale = mWebView.getScale();
+        int webViewHeight = (int) (mWebView.getContentHeight() * scale);
+        Bitmap bitmap = Bitmap.createBitmap(mWebView.getWidth(), webViewHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        mWebView.draw(canvas);
+        return bitmap;
     }
 
-    private void saveImageToGallery(Bitmap bmp) {
-        String localPath = XiuDouFangApplication.getContext().getCacheDir().getAbsolutePath();
-        File appDir = new File(localPath, "screenshot");
-        if (!appDir.exists()) {
-            appDir.mkdir();
-        }
-        String fileName = System.currentTimeMillis() + ".jpg";
-        File file = new File(appDir, fileName);
+    private void saveImageToGallery(Bitmap bitmap) {
+        //******** 品牌兼容 ********
+//        if(Build.BRAND .equals("Xiaomi") ){
+//
+//        }
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd_HHmmss", new Locale("zh", "CN"));
+        String path = Environment.getExternalStorageDirectory().getPath() + "/DCIM/Camera/";
+        String imgName =  "IMG_" + format.format(new Date()) + ".jpg";
+        File file = new File(path, imgName);
+        LogUtils.e("Brand->" + Build.BRAND + ", FilePath->" + file.getAbsolutePath());
         try {
             FileOutputStream fos = new FileOutputStream(file);
-            bmp.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
             fos.flush();
             fos.close();
         } catch (FileNotFoundException e) {
@@ -117,8 +152,9 @@ public class WebActivity extends AppCompatActivity implements IActivityBase, Vie
         } catch (IOException e) {
             e.printStackTrace();
         }
-        MediaScanner mediaScanner = new MediaScanner(this);
-        mediaScanner.scanFile(file, "image/jpeg");
+        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)));
+//        MediaScanner mediaScanner = new MediaScanner(this);
+//        mediaScanner.scanFile(file, "image/jpeg");
         // 其次把文件插入到系统图库
 //        try {
 //            String path = MediaStore.Images.Media.insertImage(getContentResolver(), file.getAbsolutePath(), fileName, null);
@@ -128,7 +164,7 @@ public class WebActivity extends AppCompatActivity implements IActivityBase, Vie
 //        }
         // 最后通知图库更新
         //String path = Environment.getExternalStorageDirectory().getPath();
-        //sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + path)));
+        //sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)));
     }
 
     @Override
